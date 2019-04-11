ec5f09f4db2ad1a756e923b399efc2b3784abdd0
Christian Sanchez <csanch1@a-018818.ups.u-psud.fr>
2019-04-11 12:44:11 +0200

    Ajout d'images pour : - Perso - Bombe - Explosion - Herbe - Boite + chargement JS

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ It's NOT Bomberman
 
 Sources for images :
 - [Walls](https://opengameart.org/content/16x16-pixel-art-dungeon-wall-and-cobblestone-floor-tiles)
+- [Box and explosions](https://opengameart.org/content/bomb-party-the-second-expansion)
+- [Bomb](https://opengameart.org/content/oldschool-bomb)
+- [Character](https://opengameart.org/content/base-character-spritesheet-16x16)
--- main.js
+++ main.js
@@ -9,12 +9,37 @@ async function loadImages() {
         "wallLeft",
         "wallRight",
         "wallCornerLeft",
-        "wallCornerRight"
+        "wallCornerRight",
+        "ground",
+        "box",
+        "bomb",
+        "bombFuse"
     ];
     for (const name of imageNames) { //Essayer de voir l'utilisation de promesses pour attendre le chargement de l'image
         images[name] = await loadImage(name);
     }
+    images = await loadCharImages(images);
+    images = await loadExplImages(images);
 
+    return images;
+}
+
+async function loadCharImages(images)
+{
+    let dir = ["up", "down", "right", "left"];
+    for (const name of dir) {
+        images["char" + name] = await loadImage("char/" + name);
+        images["char" + name + "W"] = await loadImage("char/" + name + "W");
+    }
+    return images;
+}
+
+async function loadExplImages(images)
+{
+    let dir = ["up", "down", "right", "left", "horiz", "vert", "center"];
+    for (const name of dir) {
+        images["expl" + name] = await loadImage("explosion/" + name);
+    }
     return images;
 }
 
